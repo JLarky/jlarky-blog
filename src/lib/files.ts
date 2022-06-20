ca2894da3f936a0e3365a8323127bc97467afa6d
JLarky <jlarky@gmail.com>
2022-06-19 22:50:26 -0600

    /contacts page with favicon and no layout

---
+++ src/lib/files.ts
@@ -0,0 +1,20 @@
+import { formatISO } from 'date-fns'
+import fs from 'fs'
+import path from 'path'
+
+console.log(process.cwd())
+const pagesDirectory = path.join(process.cwd(), 'src/pages')
+
+export async function getFileLastModified(fileName: string) {
+  const fullPath = path.join(pagesDirectory, fileName)
+  return new Promise<string>((resolve, reject) => {
+    fs.stat(fullPath, function (err, stats) {
+      if (err) {
+        reject(err)
+      } else {
+        const mtime = stats.mtime
+        resolve(formatISO(mtime))
+      }
+    })
+  })
+}
