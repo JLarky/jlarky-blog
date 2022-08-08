a7f76cea726008a44114a59880a6d8c241d5817f
JLarky <jlarky@gmail.com>
2022-08-07 22:40:00 -0600

    add google analytics with partytown and gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 })
