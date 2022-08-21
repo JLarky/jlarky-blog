98d358fa787fab76f839070b0c2e32a85a317fa9
JLarky <jlarky@gmail.com>
2022-08-20 20:37:19 -0600

    nx astro add image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
         forward: ['dataLayer.push'],
       },
     }),
+    image(),
   ],
 })
