5eec564b2c5f1d8a5185583b228afc9fd2a2cce6
JLarky <jlarky@gmail.com>
2023-06-25 19:10:55 -0600

    fix and configure tailwind
    - https://fogbender.com/blog/customizing-tailwind-css-in-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
-import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
     partytown({
       config: {
         forward: ['dataLayer.push'],
       },
     }),
-    image(),
   ],
 })
